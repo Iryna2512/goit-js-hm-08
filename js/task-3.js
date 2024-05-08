79426d7605fa4b9d9195d48e7dc4e61495745ef2
Iryna2512 <152912307+Iryna2512@users.noreply.github.com>
2024-05-08 18:54:28 +0300

    Homework 8

--- js/task-3.js
+++
@@ -1,7 +0,0 @@
-const nameInput = document.getElementById("name-input");
-const nameOutput = document.getElementById("name-output");
-
-nameInput.addEventListener("input", (event) => {
-  let inputText = event.target.value.trim();
-  nameOutput.textContent = inputText || "Anonymous";
-});
